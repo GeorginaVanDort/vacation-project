2edef32239d6ee3e55687f4e21765258b0d1cf3b
Georgina Van Dort <georginavandort@gmail.com>
2016-06-03 14:42:52 -0700

    add elseif statements to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,33 @@ $(document).ready(function() {
     var fun = $("#q4").val();
     var bevvy = $("#q5").val();
 
-    alert(snakes+temp);
-  });
+    if (snakes === "yes") {
+      $(".result").hide();
+      $("#kakadu").show();
+    }
+      else if (bevvy === "wine") {
+        $(".result").hide();
+        $("#barossa").show();
+    }
+      else if (fun === "crocodile") {
+        $(".result").hide();
+        $("#kakadu").show();
+    }
+      else if (temp !== "cold" && environment === "coast") {
+        $(".result").hide();
+        $("#gold").show();
+    }
+      else if (temp !== "cold" && fun === "hike") {
+        $(".result").hide();
+        $("#daintree").show();
+    }
+      else if (temp === "hot" && environment === "desert") {
+        $(".result").hide();
+        $("#kakadu").show();
+    }
+      else if (temp !== "hot" && bevvy !== "wine") {
+        $(".result").hide();
+        $("#melbs").show();
+      }
+    });
 });
