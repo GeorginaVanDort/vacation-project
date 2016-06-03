0a4bda5f2db51a53d08ef60c2374a6706a3ecbbb
Georgina Van Dort <georginavandort@gmail.com>
2016-06-03 11:23:50 -0700

    define some vars in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
+var snakes = $("input:radio[name=q1]:checked").val();
+var temp = $("#q2").val();
+var environment = $("#q3").val();
+var fun = $("#q4").val();
+var bevvy = $("#q5").val();
+
 $(document).ready(function(){
 
   $("btn-primary#ok").submit(function(event) {
